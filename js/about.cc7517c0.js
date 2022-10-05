8ecd2993124db56585ce17b0298222b93d03a9b1
Lidyaboshra <51187784+Lidyaboshra@users.noreply.github.com>
2022-10-05 03:16:08 +0200

    del

--- js/about.cc7517c0.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkecommerce_template"]=self["webpackChunkecommerce_template"]||[]).push([[443],{2323:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var u=function(){var e=this;e._self._c;return e._m(0)},s=[function(){var e=this,t=e._self._c;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an about page")])])}],a=n(1001),c={},l=(0,a.Z)(c,u,s,!1,null,null,null),r=l.exports}}]);
-//# sourceMappingURL=about.cc7517c0.js.map
